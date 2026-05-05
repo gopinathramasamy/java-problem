845f765cb97f827517e628181817395f1c0d37f8
gopinath <rgopi7145@gmail.com>
2026-05-05 21:33:03 +0530

    Greatest Common Divisor of Strings

---
+++ GreatestCommonDivisorofStrings.java
@@ -0,0 +1,44 @@
+// LeetCode : 1071. Greatest Common Divisor of Strings
+
+// For two strings s and t, we say "t divides s" if and only if s = t + t + t + ... + t + t (i.e., t is concatenated with itself one or more times).
+
+// Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+
+// Example 1:
+
+// Input: str1 = "ABCABC", str2 = "ABC"
+
+// Output: "ABC"
+
+// Example 2:
+
+// Input: str1 = "ABABAB", str2 = "ABAB"
+
+// Output: "AB"
+
+// Example 3:
+
+// Input: str1 = "LEET", str2 = "CODE"
+
+// Output: ""
+
+// Example 4:
+
+// Input: str1 = "AAAAAB", str2 = "AAA"
+
+// Output: ""​
+public class GreatestCommonDivisorofStrings{
+  public String gcdOfStrings(String str1, String str2) {
+        if(!(str1+str2).equals(str2+str1)){
+            return "";
+        }
+        int n=str1.length();
+        int m=str2.length();
+        while(m!=0){
+        int temp=m;
+        m=n%m;
+        n=temp;
+        }
+        return str1.substring(0,n);
+    }
+}
