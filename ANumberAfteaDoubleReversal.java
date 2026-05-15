d6aecf2266042b2ef4e9d92d40cab3c04f2d7531
gopinath <rgopi7145@gmail.com>
2026-05-15 21:25:14 +0530

    Smallest Even Multiple

--- ANumberAfteaDoubleReversal.java
+++ ANumberAfteaDoubleReversal.java
@@ -5,7 +5,6 @@
 // For example, reversing 2021 gives 1202. Reversing 12300 gives 321 as the leading zeros are not retained.
 // Given an integer num, reverse num to get reversed1, then reverse reversed1 to get reversed2. Return true if reversed2 equals num. Otherwise return false.
 
-
 // Example 1:
 
 // Input: num = 526
