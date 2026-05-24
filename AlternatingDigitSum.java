3d190c7e33490615bdcba2d58b632b5d3c2a5714
gopinath <rgopi7145@gmail.com>
2026-05-24 21:13:33 +0530

    Alternating Digit Sum

---
+++ AlternatingDigitSum.java
@@ -0,0 +1,47 @@
+// LeetCode : 2544. Alternating Digit Sum
+
+// You are given a positive integer n. Each digit of n has a sign according to the following rules:
+
+// The most significant digit is assigned a positive sign.
+// Each other digit has an opposite sign to its adjacent digits.
+// Return the sum of all digits with their corresponding sign.
+
+
+// Example 1:
+
+// Input: n = 521
+// Output: 4
+// Explanation: (+5) + (-2) + (+1) = 4.
+
+// Example 2:
+
+// Input: n = 111
+// Output: 1
+// Explanation: (+1) + (-1) + (+1) = 1.
+// Example 3:
+
+// Input: n = 886996
+// Output: 0
+// Explanation: (+8) + (-8) + (+6) + (-9) + (+9) + (-6) = 0.
+ 
+
+// Constraints:
+
+// 1 <= n <= 109
+
+
+public class AlternatingDigitSum {
+  public int alternateDigitSum(int n) {
+        int sum=0;
+        String str=String.valueOf(n);
+        for(int i=0;i<str.length();i++){
+            int value=str.charAt(i)-'0';
+            if(i%2==0){
+                sum = sum+value;
+            }else{
+                sum = sum-value;
+            }
+        }
+        return sum;
+    }
+}
