01d26840a3c6214b32aeabed6a7783c8ec2f9f3d
gopinath <rgopi7145@gmail.com>
2026-06-02 21:57:24 +0530

    Rotate Array

---
+++ RotateArray.java
@@ -0,0 +1,46 @@
+// Leetcode : 189. Rotate Array
+
+// Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+// Example 1:
+
+// Input: nums = [1,2,3,4,5,6,7], k = 3
+// Output: [5,6,7,1,2,3,4]
+// Explanation:
+// rotate 1 steps to the right: [7,1,2,3,4,5,6]
+// rotate 2 steps to the right: [6,7,1,2,3,4,5]
+// rotate 3 steps to the right: [5,6,7,1,2,3,4]
+// Example 2:
+
+// Input: nums = [-1,-100,3,99], k = 2
+// Output: [3,99,-1,-100]
+// Explanation: 
+// rotate 1 steps to the right: [99,-1,-100,3]
+// rotate 2 steps to the right: [3,99,-1,-100]
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// -231 <= nums[i] <= 231 - 1
+// 0 <= k <= 105
+
+class Solution {
+  public void rotate(int[] nums, int k) {
+    k = k % nums.length;
+    r(nums, 0, nums.length - 1);
+    r(nums, 0, k - 1);
+    r(nums, k, nums.length - 1);
+  }
+
+  public void r(int[] nums, int s, int e) {
+    while (s < e) {
+      int temp = nums[s];
+      nums[s] = nums[e];
+      nums[e] = temp;
+      s++;
+      e--;
+
+    }
+
+  }
+}
