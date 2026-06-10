0dd0ddd66815bb9a77afe356c251ed6835c12a7a
gopinath <rgopi7145@gmail.com>
2026-06-10 21:51:17 +0530

    First Missing Positive

---
+++ FirstMissingPositive.java
@@ -0,0 +1,53 @@
+// LeetCode : 41. First Missing Positive
+
+// Given an unsorted integer array nums. Return the smallest positive integer that is not present in nums.
+
+// You must implement an algorithm that runs in O(n) time and uses O(1) auxiliary space.
+
+// Example 1:
+
+// Input: nums = [1,2,0]
+// Output: 3
+// Explanation: The numbers in the range [1,2] are all in the array.
+// Example 2:
+
+// Input: nums = [3,4,-1,1]
+// Output: 2
+// Explanation: 1 is in the array but 2 is missing.
+// Example 3:
+
+// Input: nums = [7,8,9,11,12]
+// Output: 1
+// Explanation: The smallest positive integer 1 is missing.
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// -231 <= nums[i] <= 231 - 1
+
+
+public class FirstMissingPositive {
+  public int firstMissingPositive(int[] nums) {
+        int i=0;
+        while(i<nums.length){
+            int cor=nums[i]-1;
+            if(nums[i]>=1 && nums[i]<=nums.length && nums[i]!=nums[cor]){
+                swap(nums,i,cor);
+            }else{
+                i++;
+            }
+        }
+        for(int j=0;j<nums.length;j++){
+            if(nums[j]!=j+1){
+                return j+1;
+            }
+        }
+        return nums.length+1;
+    }
+    public  void swap(int[] nums,int first,int second){
+        int temp=nums[first];
+        nums[first]=nums[second];
+        nums[second]=temp;
+    }
+}
