15d3dd79f4579cfecea92b7ed4e621d295e3914f
GOPINATH R <rgopi7145@gmail.com>
2026-08-11 22:22:55 +0530

    Time: 0 ms (100%), Space: 43 MB (99.87%) - LeetHub

---
+++ 1122-relative-sort-array/1122-relative-sort-array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        
+        int[] freq = new int[1001];
+        for (int num : arr1) {
+            freq[num]++;
+        }
+
+        int k = 0;
+        int[] ans = new int[arr1.length];
+        for (int num : arr2) {
+            while (freq[num] > 0) {
+                ans[k++] = num;
+                freq[num]--;
+            }
+        }
+        for (int num = 0; num <= 1000; num++) {
+            while (freq[num] > 0) {
+                ans[k++] = num;
+                freq[num]--;
+            }
+        }
+        return ans;
+    }
+}
