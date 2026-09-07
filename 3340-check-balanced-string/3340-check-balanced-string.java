ebf76ee89d9df18a58c80c8b66500d0ec9a7e9f9
GOPINATH R <rgopi7145@gmail.com>
2026-09-07 20:57:46 +0530

    Time: 1 ms (97.98%), Space: 43.5 MB (67.47%) - LeetHub

---
+++ 3340-check-balanced-string/3340-check-balanced-string.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isBalanced(String num) {
+       int odd=0;
+       int even=0;
+       for(int i=0;i<num.length();i++){
+         if(i%2==0){
+            even+=num.charAt(i)-'0';
+         }else{
+            odd+=num.charAt(i)-'0';
+         }
+       }
+       if(even==odd){
+          return true;
+       }
+       return false;
+    }
+}
